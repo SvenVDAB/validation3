3e6f03ae89c8baf3c376cbc63b3e066bea828278
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-02-23 14:49:30 +0100

    problem isEmpty()

--- src/main/java/be/vdab/validation3/domain/Product.java
+++ src/main/java/be/vdab/validation3/domain/Product.java
@@ -1,2 +1,30 @@
-package be.vdab.validation3.domain;public class Product {
+package be.vdab.validation3.domain;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+
+public class Product {
+    @PositiveOrZero
+    @Digits(integer = 7, fraction = 2)
+    private BigDecimal aankoopPrijs;
+    @PositiveOrZero
+    @Digits(integer = 7, fraction = 2)
+    private BigDecimal verkoopPrijs;
+
+    public BigDecimal getAankoopPrijs() {
+        return aankoopPrijs;
+    }
+
+    public BigDecimal getVerkoopPrijs() {
+        return verkoopPrijs;
+    }
+
+    public void setAankoopPrijs(BigDecimal aankoopPrijs) {
+        this.aankoopPrijs = aankoopPrijs;
+    }
+
+    public void setVerkoopPrijs(BigDecimal verkoopPrijs) {
+        this.verkoopPrijs = verkoopPrijs;
+    }
 }
--- src/test/java/be/vdab/validation3/domain/ProductTest.java
+++ src/test/java/be/vdab/validation3/domain/ProductTest.java
@@ -1,2 +1,45 @@
-package be.vdab.validation3.domain;public class ProductTest {
+package be.vdab.validation3.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ProductTest {
+    private Validator validator;
+    private Product product;
+    @BeforeEach
+    void BeforeEach() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+        product = new Product();
+        product.setAankoopPrijs(BigDecimal.ONE);
+        product.setVerkoopPrijs(BigDecimal.TEN);
+    }
+
+    @Test
+    void correctProduct() {
+        assertThat(validator.validate(product)).isEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "-1.234", "12345678"})
+    void verkeerdeAankoopPrijzen(String prijs) {
+        product.setAankoopPrijs(new BigDecimal(prijs));
+        assertThat(validator.validate(product)).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-10", "10.234", "12345678"})
+    void verkeerdeVerkoopPrijzen(String prijs) {
+        product.setVerkoopPrijs(new BigDecimal(prijs));
+        assertThat(validator.validate(product)).isNotEmpty();
+    }
 }
